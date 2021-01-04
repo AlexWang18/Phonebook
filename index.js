1ecdbbb22a6584ddf6366a96760889dad02524df
Alex Wang <alw245@pitt.edu>
2021-01-04 13:15:56 -0500

    started

---
+++ index.js
@@ -0,0 +1,20 @@
+const persons = [
+    {
+        id: 1,
+        name: "Alex Wang",
+        number: "724-986-8135"
+    },
+    {
+        id: 2,
+        name: "Bob Ross",
+        number: "489-565-3331"
+    },
+    {
+        id: 3,
+        name: "Jim Jones",
+        number: "122-773-4532"
+    }
+]
+
+
+console.log(persons)
