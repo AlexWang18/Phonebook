a10aaf62862217a301545edefc21249aa3809f2a
Alex Wang <alw245@pitt.edu>
2021-01-05 21:40:05 -0500

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Phonebook application using Node.js for backend and React for interactive front end
+# Phonebook application using Node.js React 
+I copied my front end code from part 2 which utilized React and axios (for HTTP promises) into a build folder and put it inside this Node.js project. I then used Express to set up my backend server and pseudo database with CORS middleware to permit the fetching of data from our backend through XMLHttpRequests.
 
+Website:
 https://thawing-beach-50829.herokuapp.com/
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const express = require('express')
 const app = express()
 
 
-app.use(cors())
+app.use(cors()) //i hate life
 app.use(express.json())  //transforms json into a JS object that is attached to body of the request b4 route handler is called, "middleware" in that it handles req, res objects
 app.use(express.static('build'))
 
@@ -102,6 +102,5 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-console.log('why is the page not loading');
-console.log('so annoying')
+
 //console.log(persons) this would run before any of the route handlers or server creation>
